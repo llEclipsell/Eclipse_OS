d272b34c1c4be1b897380cc34fa500b7d5179bde
llEclipsell <pradyun2611@gmail.com>
2026-07-28 22:28:00 +0000

    Meaty Skeleton restructure with customizations ported

--- build.sh
+++ build.sh
@@ -1,9 +1,7 @@
-#!/bin/bash
-set -e	# stop immediately if any command fails
+#!/bin/sh
+set -e
+. ./headers.sh
 
-i686-elf-gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
-i686-elf-gcc -T linker.ld -o myos -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
-grub-file --is-x86-multiboot myos && echo multiboot confirmed
-cp myos isodir/boot/myos
-grub-mkrescue -o myos.iso isodir
-qemu-system-i386 -cdrom myos.iso -display curses
+for PROJECT in $PROJECTS; do
+  (cd $PROJECT && DESTDIR="$SYSROOT" $MAKE install)
+done
---
+++ kernel/arch/i386/tty.c
@@ -0,0 +1,85 @@
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <string.h>
+
+#include <kernel/tty.h>
+
+#include "vga.h"
+
+static const size_t VGA_WIDTH = 80;
+static const size_t VGA_HEIGHT = 25;
+static uint16_t* const VGA_MEMORY = (uint16_t*) 0xB8000;
+
+static size_t terminal_row;
+static size_t terminal_column;
+static uint8_t terminal_color;
+static uint16_t* terminal_buffer;
+
+void terminal_initialize(void) {
+	terminal_row = 0;
+	terminal_column = 0;
+	terminal_color = vga_entry_color(VGA_COLOR_LIGHT_GREY, VGA_COLOR_BLACK);
+	terminal_buffer = VGA_MEMORY;
+	for (size_t y = 0; y < VGA_HEIGHT; y++) {
+		for (size_t x = 0; x < VGA_WIDTH; x++) {
+			const size_t index = y * VGA_WIDTH + x;
+			terminal_buffer[index] = vga_entry(' ', terminal_color);
+		}
+	}
+}
+
+void terminal_setcolor(uint8_t color) {
+	terminal_color = color;
+}
+
+void terminal_putentryat(unsigned char c, uint8_t color, size_t x, size_t y) {
+	const size_t index = y * VGA_WIDTH + x;
+	terminal_buffer[index] = vga_entry(c, color);
+}
+
+static void terminal_scroll(void){
+        /* Shift every row up by one: row y takes row y+1's content */
+        for(size_t x = 0; x < VGA_HEIGHT - 1; x++){
+                for(size_t y = 0; y < VGA_WIDTH; y++){
+                        terminal_buffer[x * VGA_WIDTH + y] = terminal_buffer[(x + 1) * VGA_WIDTH + y];
+                }
+        }
+
+        /* Clear the last row, ready for new input */
+        for(size_t x = 0; x < VGA_WIDTH; x++){
+                terminal_buffer[(VGA_HEIGHT - 1) * VGA_WIDTH + x] = vga_entry(' ', terminal_color);
+        }
+}
+
+void terminal_putchar(char c) {
+        if(c == '\n'){
+                terminal_column = 0;
+                /* The if condn is being checked eachtime and ++term_row is being triggered always eitherway >
+                rows are not overflowing*/
+                if(++terminal_row == VGA_HEIGHT){
+                        terminal_scroll();
+                        terminal_row = VGA_HEIGHT - 1;
+                }
+                return;
+        }
+
+	unsigned char uc = c;
+	terminal_putentryat(uc, terminal_color, terminal_column, terminal_row);
+	if(++terminal_column == VGA_WIDTH){
+                terminal_column = 0;
+                if(++terminal_row == VGA_HEIGHT){
+                        terminal_scroll();
+                        terminal_row = VGA_HEIGHT - 1;
+                }
+        }
+}
+
+void terminal_write(const char* data, size_t size) {
+	for (size_t i = 0; i < size; i++)
+		terminal_putchar(data[i]);
+}
+
+void terminal_writestring(const char* data) {
+	terminal_write(data, strlen(data));
+}
