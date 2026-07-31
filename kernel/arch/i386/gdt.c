b220df7e5daf66ece1bbb89cb019f25b8eb78bfd
llEclipsell <pradyun2611@gmail.com>
2026-07-31 19:46:24 +0000

    Adding of custom GDT

---
+++ kernel/arch/i386/gdt.c
@@ -0,0 +1,46 @@
+#include <stdint.h>
+#include <kernel/gdt.h>
+
+#define GDT_ENTRIES 3
+
+struct gdt_entry {
+	uint16_t limit_low;
+	uint16_t base_low;
+	uint8_t  base_middle;
+	uint8_t  access;
+	uint8_t  granularity;
+	uint8_t  base_high;
+} __attribute__((packed));
+
+struct gdt_ptr {
+	uint16_t limit;
+	uint32_t base;
+} __attribute__((packed));
+
+static struct gdt_entry gdt[GDT_ENTRIES];
+static struct gdt_ptr   gp;
+
+extern void gdt_flush(uint32_t gdt_ptr);
+
+static void gdt_set_gate(int num, uint32_t base, uint32_t limit,
+                         uint8_t access, uint8_t gran) {
+	gdt[num].base_low    =  base         & 0xFFFF;
+	gdt[num].base_middle = (base >> 16)  & 0xFF;
+	gdt[num].base_high   = (base >> 24)  & 0xFF;
+
+	gdt[num].limit_low   =  limit        & 0xFFFF;
+	gdt[num].granularity = ((limit >> 16) & 0x0F) | (gran & 0xF0);
+
+	gdt[num].access      = access;
+}
+
+void gdt_initialize(void) {
+	gp.limit = (sizeof(struct gdt_entry) * GDT_ENTRIES) - 1;
+	gp.base  = (uint32_t) &gdt;
+
+	gdt_set_gate(0, 0, 0,       0x00, 0x00);  /* null descriptor */
+	gdt_set_gate(1, 0, 0xFFFFF, 0x9A, 0xC0);  /* kernel code */
+	gdt_set_gate(2, 0, 0xFFFFF, 0x92, 0xC0);  /* kernel data */
+
+	gdt_flush((uint32_t) &gp);
+}
--- libc/ssp/ssp.c
+++ libc/ssp/ssp.c
@@ -11,9 +11,5 @@ uintptr_t __stack_chk_guard = STACK_CHK_GUARD;
 
 __attribute__((noreturn))
 void __stack_chk_fail(void) {
-#if __STDC_HOSTED__
 	abort();
-#elif __is_myos_kernel
-	panic("Stack smashing detected");
-#endif
 }
